4d0d684ae621968a7d63a2ec925240713042b70d
massar@UNIX-AG.UNI-KL.DE <massar@UNIX-AG.UNI-KL.DE@315857ad-0bdb-0310-b42e-dec37551a5f0>
2004-11-21 15:14:11 +0000

    tag previous releases

--- sysdep-linux.c
+++ sysdep-linux.c
@@ -16,6 +16,10 @@
     GNU General Public License for more details.
  */
 
+/*
+ * $Id: tun_dev.c,v 1.1.2.2 2000/11/20 08:15:53 maxk Exp $
+ */ 
+
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -34,79 +38,73 @@
 /* 
  * Allocate TUN device, returns opened fd. 
  * Stores dev name in the first arg(must be large enough).
- */
+ */  
+int tun_open_old(char *dev)
+{
+    char tunname[14];
+    int i, fd;
+
+    if( *dev ) {
+       sprintf(tunname, "/dev/%s", dev);
+       return open(tunname, O_RDWR);
+    }
+
+    for(i=0; i < 255; i++){
+       sprintf(tunname, "/dev/tun%d", i);
+       /* Open device */
+       if( (fd=open(tunname, O_RDWR)) > 0 ){
+          sprintf(dev, "tun%d", i);
+          return fd;
+       }
+    }
+    return -1;
+}
+
 #ifdef IFF_TUN
 int tun_open(char *dev)
 {
-	struct ifreq ifr;
-	int fd, err;
+    struct ifreq ifr;
+    int fd, err;
 
-	if ((fd = open("/dev/net/tun", O_RDWR)) < 0) {
-		error(0, errno,
-			"can't open /dev/net/tun, check that it is either device char 10 200 or (with DevFS) a symlink to ../misc/net/tun (not misc/net/tun)");
-		return -1;
-	}
+    if( (fd = open("/dev/net/tun", O_RDWR)) < 0 )
+       return tun_open_old(dev);
 
-	memset(&ifr, 0, sizeof(ifr));
-	ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
-	if (*dev)
-		strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+    memset(&ifr, 0, sizeof(ifr));
+    ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
+    if( *dev )
+       strncpy(ifr.ifr_name, dev, IFNAMSIZ);
 
-	if ((err = ioctl(fd, TUNSETIFF, (void *)&ifr)) < 0) {
-		close(fd);
-		return err;
-	}
-	strcpy(dev, ifr.ifr_name);
-	return fd;
+    if( (err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0 ){
+       close(fd);
+       return err;
+    } 
+    strcpy(dev, ifr.ifr_name);
+    return fd;
 }
 #else
 int tun_open(char *dev)
 {
-	char tunname[14];
-	int i, fd;
-
-	if (*dev) {
-		if (strncmp(dev, "tun", 3))
-			error(1, 0,
-				"error: arbitrary naming tunnel interface is not supported in this version\n");
-		sprintf(tunname, "/dev/%s", dev);
-		return open(tunname, O_RDWR);
-	}
-
-	for (i = 0; i < 255; i++) {
-		sprintf(tunname, "/dev/tun%d", i);
-		/* Open device */
-		if ((fd = open(tunname, O_RDWR)) > 0) {
-			sprintf(dev, "tun%d", i);
-			return fd;
-		}
-	}
-	return -1;
+	return tun_open_old(dev);
 }
-
 #endif /* New driver support */
 
 int tun_close(int fd, char *dev)
 {
-	dev = NULL; /*unused */
-	return close(fd);
+    dev = NULL; /*unused*/
+    return close(fd);
 }
 
 /* Read/write frames from TUN device */
 int tun_write(int fd, char *buf, int len)
 {
-	return write(fd, buf, len);
+    return write(fd, buf, len);
 }
 
 int tun_read(int fd, char *buf, int len)
 {
-	return read(fd, buf, len);
+    return read(fd, buf, len);
 }
 
 /***********************************************************************/
 /* other support functions */
 
-const char *sysdep_config_script(void)
-{
-	return "ifconfig $TUNDEV inet $INTERNAL_IP4_ADDRESS pointopoint $INTERNAL_IP4_ADDRESS netmask 255.255.255.255 mtu 1412 up";
-}
