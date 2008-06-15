6e436d35e71274633adb3ea4aa8e2608b627269d
Joerg Mayer <Joerg Mayer@315857ad-0bdb-0310-b42e-dec37551a5f0>
2008-06-15 18:12:17 +0000

    Update some svn properties.

--- Makefile
+++ Makefile
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-# $Id: Makefile 278 2008-01-13 23:05:52Z Joerg Mayer $
+# $Id$
 
 DESTDIR=
 PREFIX=/usr/local
--- tunip.c
+++ tunip.c
@@ -21,7 +21,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-   $Id: tunip.c 293 2008-06-12 19:05:27Z Joerg Mayer $
+   $Id$
 */
 
 /* borrowed from pipsecd (-; */
