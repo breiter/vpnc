dc68c33289ec07db8ce22dd2d93dda3255453d8f
Antonio Borneo <Antonio Borneo@315857ad-0bdb-0310-b42e-dec37551a5f0>
2014-02-18 05:09:48 +0000

    Replace obsolete getpass()
    Function getpass(3) is reported as obsolete.
    Replace it with new vpnc_getpass().
    Differences with original implementation:
    - output prompt on stdout, instead of /dev/tty;
    - input from stdin, instead of /dev/tty;
    - password length limited by vpnc_getline() to 200 chars.
    
    Functions tcgetattr()/tcsetattr() return error if stdin
    is not a terminal but, e.g., a pipe or a file. I simply
    ignore the error, since no need to disable ECHO on them.
    
    Signed-off-by: Antonio Borneo <borneo.antonio@gmail.com>

--- vpnc.c
+++ vpnc.c
@@ -2508,14 +2508,17 @@ static int do_phase2_xauth(struct sa_block *s)
 						|| ap->type == ISAKMP_XAUTH_02_ATTRIB_USER_PASSWORD) ?
 						"Password" : "Passcode",
 						config[CONFIG_XAUTH_USERNAME], ntop_buf);
-					pass = getpass(prompt);
+					pass = vpnc_getpass(prompt);
 					free(prompt);
+					if (pass == NULL)
+						error(2, 0, "unable to get password");
 
 					na = new_isakmp_attribute(ap->type, NULL);
 					na->u.lots.length = strlen(pass);
 					na->u.lots.data = xallocc(na->u.lots.length);
 					memcpy(na->u.lots.data, pass, na->u.lots.length);
 					memset(pass, 0, na->u.lots.length);
+					free(pass);
 				} else {
 					if (opt_vendor == VENDOR_NORTEL
 					    && opt_auth_mode != AUTH_MODE_NORTEL_GPASSWORD)
